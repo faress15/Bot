3404a749367be8d9b6d13b8c99c7e0c23d1bebe5
fares <darabfares@gmail.com>
2025-08-02 16:21:05 +0330

    update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-BOT_TOKEN = '7571396324:AAEzZf7Z6IkFQJScat4I88Ilpb2Pd6MzYPA'
-PISTON_URL = "https://emkc.org/api/v2/piston/execute"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+PISTON_URL = os.getenv("PISTON_URL", "https://emkc.org/api/v2/piston/execute")
